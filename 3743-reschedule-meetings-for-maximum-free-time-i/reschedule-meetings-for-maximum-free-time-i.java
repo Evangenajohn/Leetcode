3a9991af7a2ec4cbebdada27f5c6c892bee29c9b
Evangelin John <evangenajohn17@gmail.com>
2025-07-09 23:37:33 +0530

    Time: 2 ms (76.11%) | Memory: 63.2 MB (53.33%) - LeetSync

--- 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.java
+++ 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.java
@@ -1,21 +1,16 @@
-class Solution {
+public class Solution {
+
     public int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
-        int count = startTime.length;
-        int[] prefixSum = new int[count + 1];
-        int maxFree = 0;
-
-        for (int i = 0; i < count; i++) {
-            prefixSum[i + 1] = prefixSum[i] + endTime[i] - startTime[i];
+        int n = startTime.length, res = 0;
+        int[] sum = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            sum[i + 1] = sum[i] + endTime[i] - startTime[i];
         }
-
-        for (int i = k - 1; i < count; i++) {
-            int occupied = prefixSum[i + 1] - prefixSum[i - k + 1];
-            int windowEnd = (i == count - 1) ? eventTime : startTime[i + 1];
-            int windowStart = (i == k - 1) ? 0 : endTime[i - k];
-            int freeTime = windowEnd - windowStart - occupied;
-            maxFree = Math.max(maxFree, freeTime);
+        for (int i = k - 1; i < n; i++) {
+            int right = i == n - 1 ? eventTime : startTime[i + 1];
+            int left = i == k - 1 ? 0 : endTime[i - k];
+            res = Math.max(res, right - left - (sum[i + 1] - sum[i - k + 1]));
         }
-
-        return maxFree;
+        return res;
     }
 }
