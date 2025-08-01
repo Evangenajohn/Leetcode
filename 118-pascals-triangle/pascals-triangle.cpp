639fd81ef6f0550a4efd496c1021acfa47afd14d
Evangelin John <evangenajohn17@gmail.com>
2025-08-01 07:15:13 +0530

    Time: 2 ms (15.16%) | Memory: 10.6 MB (5.38%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> a(numRows);
-        for(int&& i=0; i<numRows; i++){
-            a[i].assign(i+1, 1);// exact allocation once
-            for(int&& j=1; j<=i/2; j++){
-                  a[i][i-j]=a[i][j]=a[i-1][j-1]+a[i-1][j];
-                        
-            }              
+        if (numRows == 0) return {};
+        if (numRows == 1) return {{1}};
+        
+        vector<vector<int>> prevRows = generate(numRows - 1);
+        vector<int> newRow(numRows, 1);
+        
+        for (int i = 1; i < numRows - 1; i++) {
+            newRow[i] = prevRows.back()[i - 1] + prevRows.back()[i];
         }
-        return a;
+        
+        prevRows.push_back(newRow);
+        return prevRows;
     }
 };
