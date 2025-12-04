ac1fba4e9c48ca42c2387ced914d21b203c4b103
Evangelin John <evangenajohn17@gmail.com>
2025-12-04 22:00:06 +0530

    Time: 11 ms (18.61%) | Memory: 12.9 MB (98.06%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,17 +1,33 @@
-class Solution(object):
+class Solution:
     def setZeroes(self, matrix):
-        zeroinFirstCol = False
-        for row in range(len(matrix)):
-            if matrix[row][0] == 0:
-                zeroinFirstCol = True
-            for col in range(1, len(matrix[0])):
-                if matrix[row][col] == 0:
-                    matrix[row][0] = 0
-                    matrix[0][col] = 0
+        rows = len(matrix)
+        cols = len(matrix[0])
+        first_row_zero = False
+        first_col_zero = False
 
-        for row in range(len(matrix) - 1, -1, -1):
-            for col in range(len(matrix[0]) - 1, 0, -1):
-                if matrix[row][0] == 0 or matrix[0][col] == 0:
-                    matrix[row][col] = 0
-            if zeroinFirstCol:
-                matrix[row][0] = 0
+        for j in range(cols):
+            if matrix[0][j] == 0:
+                first_row_zero = True
+
+        for i in range(rows):
+            if matrix[i][0] == 0:
+                first_col_zero = True
+
+        for i in range(1, rows):
+            for j in range(1, cols):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+
+        for i in range(1, rows):
+            for j in range(1, cols):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+
+        if first_row_zero:
+            for j in range(cols):
+                matrix[0][j] = 0
+
+        if first_col_zero:
+            for i in range(rows):
+                matrix[i][0] = 0
